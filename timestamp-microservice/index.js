17a1e33a019322e2a72377a6697f6b17f1e737c7
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-09-23 16:56:31 +0530

    first commit

---
+++ timestamp-microservice/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
+
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date)
+  }
+
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
+    return;
+  }
+  
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
+
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
